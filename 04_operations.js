c53c51f13c5d70255d3a7c0c0d00c312459462ad
DeepSarkar1221 <1221sarkardeep1221@gmail.com>
2026-05-08 01:32:03 +0530

    some operations

---
+++ 04_operations.js
@@ -0,0 +1,49 @@
+
+console.log("1"+2); //12 (string)
+
+console.log("1"+2+1);  //121 
+console.log(2+"1");  //21
+console.log(2+1+"1");  //31
+
+let s1="Deep"
+let s2=" Sarkar"
+console.log(s1+s2);  // Deep Sarkar
+
+console.log(true); //true
+console.log(+true); //1
+console.log(""); // 
+console.log(+""); //0
+
+let num1,num2,num3
+num1=num2=num3=2+2
+console.table([num2,num3,num1])
+
+
+num1++;
+console.log(num1); //5
+
+console.log(NaN+NaN); //NaN
+console.log(NaN+undefined); //NaN
+console.log(null+null);//0
+console.log(NaN+null);// NaN
+console.log(null+undefined); //NaN
+console.log(undefined+undefined); //NaN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
