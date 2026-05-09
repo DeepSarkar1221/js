efd84b9b9a1f239d85c9479bd1c90981f5a9a68c
DeepSarkar1221 <1221sarkardeep1221@gmail.com>
2026-05-09 16:42:22 +0530

    adding

---
+++ 05_comparison.js
@@ -0,0 +1,24 @@
+
+
+
+console.log("2">1); //true
+console.log(null==0); //false
+console.log(null>0); //false
+console.log(null>=0); //true    bcz equality check(==) doesnot convert the null into 0 but the comparison operators(e.g. >,<,>=,<=) changes it into 0
+console.log(null<=0); //true
+
+
+
+console.log(undefined==0); //false
+console.log(undefined>=0); //false (for any comparison operation)
+
+
+//***************  === ***********************/
+
+console.log("2"=== 2); //false (bcz it also check data Type)
+
+
+
+
+
+
---
+++ 06_more_abt_string.js
@@ -0,0 +1,24 @@
+
+
+let name= new String("deep007") // as a object
+console.log(name);
+let n="deepSarkar"
+let n2=n.slice(-9,4)
+console.log(n2);
+console.log(n.substring(0,3));
+console.log(n.replace('e','r')); // replace the 1st e with r
+console.log(n.includes('e'));// return true if it have e
+
+name="Deep-Sarkar-Is-a-legend"
+console.log(name.split('-'));
+
+
+
+
+
+
+
+
+
+
+
